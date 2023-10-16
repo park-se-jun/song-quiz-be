6240a09546cd5e95fa2e52385d7f81796cbcd4d9
박세준 <tpwns6693@gmail.com>
2023-10-16 09:51:04 +0900

    first commit

---
+++ src/main/java/com/example/songquiz/SongquizApplication.java
@@ -0,0 +1,13 @@
+package com.example.songquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/songquiz/SongquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.songquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
